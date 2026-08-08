5223010fd97b7623d75cc1877e81212f6698123c
Sumit Bhoi <sumitbhoi2839@gmail.com>
2026-08-09 01:01:25 +0530

    Update server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -89,5 +89,5 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server Running : http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
